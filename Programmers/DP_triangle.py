e3c82da96023f7fd7998806b1c4569741cc57240
vhehduatks <hegyna11@gmail.com>
2020-10-25 00:23:27 +0900

    kakao code

--- Programmers/DP_triangle.py
+++ Programmers/DP_triangle.py
@@ -2,7 +2,7 @@ triangle=[[7], [3, 8], [8, 1, 0], [2, 7, 4, 4], [4, 5, 2, 6, 5]]
 
 def solution(triangle):
     sum=[[0]*len(line) for line in triangle]
-    sum[0][0]=triangle[0][0]
+
     for i in range(len(triangle)):
         for j in range(len(triangle[i])):
             if(j==0):
