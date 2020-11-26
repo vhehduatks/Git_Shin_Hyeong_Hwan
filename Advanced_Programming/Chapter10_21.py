fc23232d48e81c742bfe9f8bcff8855046c2aebc
vhehduatks <hegyna11@gmail.com>
2020-11-26 20:12:11 +0900

    modf

--- Advanced_Programming/Chapter10_21.py
+++ Advanced_Programming/Chapter10_21.py
@@ -15,4 +15,9 @@ reverse_generator2=lambda x:reversed(x)
 #람다식 구현시에 filter(조건,iter객체)는 참인 조건(즉 조건문이 필요)을 묶어서 리턴함-> list로 형변환 필요
 # lambda x: x~~은 x를 매개변수로 받아서 return x~~이란 뜻   
 last_odd=max(list(filter(lambda x:x%2!=0,list(reverse_generator2(test_case)))))
+
+#better way
+#람다는 매게변수를 가진 함수임
+last_odd2=lambda x: max([i for i in reversed(x) if i%2!=0])
 print("\n",last_odd)
+print(last_odd2(test_case))
--- Advanced_Programming/Chapter10_25.py
+++ Advanced_Programming/Chapter10_25.py
@@ -4,6 +4,7 @@
 is_prime=lambda x: all(x%i!=0 for i in range(2,int(x/2)))
 
 print(is_prime(12))
+print(is_prime(11))
 
 prime_nums=lambda m,n: list(filter(is_prime,[i for i in range(m,n+1)]))
 print(prime_nums(10,50))
